22187e07666c89ff79decadd50b8f8d1c3d57f20
Sanchit Gadekar <38809433+Sanchit-Gadekar@users.noreply.github.com>
2024-01-05 14:36:34 +0530

    Initial Commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -11,14 +11,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var storyLabel: UILabel!
+    
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
     }
-
+    
+    //button to use its title for updation
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+     
+    }
+    
+    //update ui elements everytime choice is selected such as title,choices
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+    }
 
 }
 
---
+++ UserData.swift
@@ -0,0 +1,9 @@
+//
+//  UserData.swift
+//  Clima
+//
+//  Created by Goldmedal on 29/12/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
